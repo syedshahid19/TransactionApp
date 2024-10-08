72ecc5f40df661c2c97bf727d3fde2876b86c53d
syedshahid19 <syedshahid9550@gmail.com>
2024-10-08 14:00:23 +0530

    linking done

--- server/controllers/Transaction.js
+++ server/controllers/Transaction.js
@@ -26,7 +26,7 @@ exports.addTransaction = async (req, res) => {
     try {
       const savedTransaction = await newTransaction.save();
       // Trigger LMS webhook to send lead data
-      await axios.post('https://rolebaseapp.onrender.com/api/v1/user/createLead', leadData);
+      await axios.post('https://rolebaseapp.onrender.com/api/v1/user/createLead', savedTransaction);
       res.status(200).send('Lead captured successfully');
       // res.json(savedTransaction);
     } catch (err) {
